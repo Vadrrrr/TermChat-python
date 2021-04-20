0d1c9207253a431aa562ae19fb4d5d497e2f88b7
Vadym Khorkov <vkhorkov@e1r12p1.unit.ua>
2021-04-20 21:32:13 +0300

    TermChat

---
+++ client/client.py
@@ -0,0 +1,87 @@
+import socket
+import threading
+import sys
+import pickle
+from colorama import init, Fore, Style
+
+class Client:
+	def __init__(self, ip, port):
+		self.ip = ip
+		self.port = port
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket.connect((self.ip, self.port))
+		self.json_register = {}
+		self.json_login = {}
+		self.register_or_login()
+
+	def register_or_login(self):
+		msg = self.socket.recv(1024).decode('utf-8')
+		print(msg)
+
+		answer = input('(1/2): ')
+		self.socket.send(bytes(answer, 'utf-8'))
+		if answer == '1':
+			self.register()
+		elif answer == '2':
+			self.login()
+
+	def register(self):
+		print('|||REGISTER|||')
+		self.json_register['login'] = input('Enter your login: ')
+		self.json_register['password'] = input('Enter your password: ')
+
+		global name
+		name = self.json_register['login']
+
+		res = pickle.dumps(self.json_register)
+		self.socket.send(res)
+
+		msg = self.socket.recv(1024).decode('utf-8')
+
+		if msg == 'User with this login exists. Try another login.':
+			print(msg)
+			self.register()
+		elif msg == 'You have been successfully registered!':
+			print(msg)
+			threading.Thread(target=self.msg_recv, daemon=True).start()
+			self.msg_send()
+
+	def login(self):
+		print('|||LOGIN|||')
+		self.json_login['login'] = input('Enter your login: ')
+		self.json_login['password'] = input('Enter your password: ')
+
+		global name
+		name = self.json_login['login']
+
+		res = pickle.dumps(self.json_login)
+		self.socket.send(res)
+
+		msg = self.socket.recv(1024).decode('utf-8')
+
+		if msg:
+			print(msg)
+			threading.Thread(target=self.msg_recv, daemon=True).start()
+			self.msg_send()
+
+	def msg_recv(self):
+		while True:
+			try:
+				msg = self.socket.recv(1024).decode('utf-8')
+				if not msg:
+					break
+				print(Fore.YELLOW)
+				print(msg)
+			except:
+				break
+
+	def msg_send(self):
+		while True:
+			try:
+				print(Fore.RED)
+				self.socket.send(bytes(name + ': ' + input(''), 'utf-8'))
+			except:
+				break
+
+
+c1 = Client('', 8000)
